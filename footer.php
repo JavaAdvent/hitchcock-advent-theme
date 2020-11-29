82a8448ce25549b94b55032a008734f6760395ba
Holger <holger@steinhauer.software>
2020-11-29 17:21:48 +0100

    Image sizes, HTML characters and path
    Besides bringing the JetBrains and steinhauer.software image to a more sensible size this will fix the image paths and use `&amp;` instead of an ambiguous `&`.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		</main><!-- #site-content -->
 
 		<div class="credits section-inner">
-            <p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> | Powered by <a href="https://www.jetbrains.com" target="_blank"><img src="/assets/images/jetbrains.png" alt="Jetbrains Logo" />JetBrains</a> & <a href="https://steinhauer.software" target="_blank"><img src="/assets/images/steinhauer.software.png" alt="steinhauer.software Logo"/>steinhauer.software</a></p>
+            <p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> | Powered by <a href="https://www.jetbrains.com" target="_blank"><img src="<?= get_template_directory_uri() ?>/assets/images/jetbrains.png" alt="Jetbrains Logo" />JetBrains</a> &amp; <a href="https://steinhauer.software" target="_blank"><img src="<?= get_template_directory_uri() ?>/assets/images/steinhauer.software.png" alt="steinhauer.software Logo"/>steinhauer.software</a></p>
 			<p class="theme-by"><?php _e( 'Theme by', 'hitchcock' ); ?> <a href="https://www.andersnoren.se">Anders Nor&eacute;n</a></p>
 		</div><!-- .credits -->
 
